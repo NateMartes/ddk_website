7e38e437106c5f5fbc55c328a0f6142fa247fcee
Nathaniel Martes <natemartes09@gmail.com>
2024-05-13 13:35:00 -0400

    added email send ablitiy using web3forms API

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* window animation */
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting){
@@ -5,6 +6,53 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 });
-
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* scroll to ids */
+function scrollAnimation(event, element){
+    console.log(element);
+    const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+    if (prefersReducedMotion){
+        return;
+    }
+    event.preventDefault();
+    const targetId = element.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth"
+    })
+}
+
+/* form error checking */
+document.getElementById("name").addEventListener("input", function(){
+    let name = this.value
+    if (name == ""){
+        this.setCustomValidity("Name Cannot Be Empty");
+    } else {
+        this.setCustomValidity("");
+    }
+});
+document.getElementById("phoneNumber").addEventListener("input", function(){
+    let regex = /^\(?[0-9]{3}\)?-?[0-9]{3}-?[0-9]{4}$/;
+    if (!regex.test(this.value)){
+        this.setCustomValidity("Ex : 123-456-0000");
+    }else {
+        this.setCustomValidity("");
+    }
+});
+document.getElementById("email").addEventListener("input", function(){
+    let regex = /^.+@.+$/;
+    if (!regex.test(this.value)){
+        this.setCustomValidity("Ex : johndoe@ddk.com");
+    } else {
+        this.setCustomValidity("");
+    }
+});
+document.getElementById("subject").addEventListener("input", function(){
+    let name = this.value
+    if (name == ""){
+        this.setCustomValidity("Name Cannot Be Empty");
+    }
+});
